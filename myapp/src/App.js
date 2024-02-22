6669eb5424b76db3fe274769cd10b46b6878fcf9
Adem Fdhili <ademdanielo@gmail.com>
2024-02-22 23:31:16 +0100

    first ex

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import image from './HOUSE.png';
 import './App.css';
 
 function App() {
   return (
     <form>
       <div class="pr">
-        <img src="https://res.cloudinary.com/dgci0ivas/image/upload/v1684534060/Image_1_g5mxyi.png" alt="image" />
+      <img src={image} alt="image" />
       </div>
       <h3>Blog post published</h3>
       <p>This blog post has been published Team members <br />will be able to edit this post and republish changes</p>
